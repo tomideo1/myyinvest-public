b61bbdc4bd28f83fab90bb77a322134dacc35d44
tomideo1 <ayotomideaina@gmail.com>
2021-01-14 16:53:27 +0100

    initial commit landing page

---
+++ src/components/Shared/bottomFooter.vue
@@ -0,0 +1,130 @@
+<template>
+  <div class="container-fluid footer-red">
+    <div class="row footer-row">
+      <div class="col-md-2 footer-col">
+        <h3>Company</h3>
+        <ul>
+          <li>
+            <a href="index.html">Home</a>
+          </li>
+          <li>
+            <a href="about.html">About Us</a>
+          </li>
+
+          <li>
+            <a href="#">Media</a>
+          </li>
+
+          <li>
+            <a href="listings.html">Listings</a>
+          </li>
+          <li>
+            <a href="#">Security</a>
+          </li>
+          <li>
+            <a href="tos.html">Terms and Conditions</a>
+          </li>
+          <li>
+            <a href="privacy-policy.html">Privacy Policy</a>
+          </li>
+          <li>
+            <a href="disclaimer.html">Disclaimer</a>
+          </li>
+        </ul>
+      </div>
+
+      <div class="col-md-2 footer-col">
+        <h3>Learn</h3>
+        <ul>
+          <li>
+            <a href="faq.html">FAQ</a>
+          </li>
+          <li>
+            <a href="insight.html">Insight</a>
+          </li>
+
+          <li>
+            <a href="#">How it works</a>
+          </li>
+
+          <li>
+            <a href="#">Podcast</a>
+          </li>
+        </ul>
+      </div>
+
+      <div class="col-md-3 footer-col">
+        <h3>Community</h3>
+        <ul>
+          <li>
+            <a href="#">Join our community</a>
+          </li>
+          <li>
+            <a href="#">Register for Webinar</a>
+          </li>
+
+          <li>
+            <a href="#">Gallery </a>
+          </li>
+        </ul>
+      </div>
+      <div class="col-md-5 footer-col">
+        <h3>CONTACT</h3>
+        <p>
+          <i class="fa fa-map-marker"></i> &nbsp 2nd floor Hub One, No.10 Hughes Avenue,<br />
+          Alagomeji Yaba, Lagos State.
+        </p>
+        <p><i class="fa fa-envelope"></i>&nbsp info@myyinvest.com.ng</p>
+        <p><i class="fa fa-phone"></i>&nbsp 07036821841, 08108933352, 08132049708</p>
+      </div>
+    </div>
+
+    <div class="final-footer">
+      <img src="images/myyivest-white-logo.png" alt="" />
+
+      <div id="footer" class="container-fluid footer-img">
+        <div class="social-icons">
+          <ul>
+            <li>
+              <a class="whatsapp" href="#"><i class="fa fa-whatsapp"></i></a>
+            </li>
+            <li>
+              <a class="twitter" href="#"><i class="fa fa-twitter"></i></a>
+            </li>
+            <li>
+              <a class="instagram" href="#"><i class="fa fa-instagram"></i></a>
+            </li>
+            <li>
+              <a class="facebook" href="#"><i class="fa fa-facebook"></i></a>
+            </li>
+            <li>
+              <a class="linkedin" href="#"><i class="fa fa-linkedin"></i></a>
+            </li>
+            <li>
+              <a class="youtube" href="#"><i class="fa fa-youtube"></i></a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+
+    <div class="aboutus-f">
+      <p>
+        Myyinvest.com Ltd is a Property Technology (PropTech) Company duly registered with the Corporate Affairs Commission (CAC) of Nigeria with RC 1655737. Investments are held in Trust by Quantum
+        Zenith Trustees, the Trustee Arm of Zenith Bank and a company Registered and Regulated by the Securities and Exchange Commission (SEC). Every investment has an insurance cover which is handled
+        by Leadway Assurance Company Limited, Authorized and Regulated by the National Insurance Commission (NIC) with RIC NO. 025. Myyinvest.com Ltd Formerly Myycrib.com.ng is a prestigious member of
+        the Fintech Nigeria Association.
+      </p>
+      <br /><br />
+      <h5>&copy 2020 MyyInvest. All rights reserved.</h5>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "bottomFooter"
+};
+</script>
+
+<style scoped></style>
--- src/views/Shared/Landing.vue
+++ src/views/Shared/Landing.vue
@@ -2,6 +2,7 @@
   <div>
     <main class="d-none d-lg-block d-md-block">
       <top-nav />
+
       <section>
         <div class="container-fluid">
           <div class="row">
@@ -17,7 +18,7 @@
               <img style="width:95%;position: absolute;right:10%;top:-5%; z-index: -1" src="@/assets/svgs/incoming.png" />
             </div>
 
-            <div class="col-lg-6 col-md-6">
+            <div class="col-lg-6 col-md-6 mt-5">
               <img style="width: 90%; position: relative; z-index: -1" src="@/assets/svgs/map.png" />
 
               <span>
@@ -59,15 +60,242 @@
 
       <section style="margin-top: 150px;" class="ml-5">
         <div class="container-fluid">
-          <span class="d-flex flex-column ">
-            <span>
-              <h3>Co - Invest</h3>
+          <div class="row ">
+            <div class="col-lg-6 col-md-12">
+              <span class="d-flex flex-column ">
+                <h3>Co - Invest</h3>
+                <span class="header-underline" style="margin-left: 150px;"></span>
+              </span>
+              <p class="w-100 mt-3  font-weight-normal font-poppins" style="line-height:40px">
+                Earn short-term returns when you invest in our vetted fractional real estate’s project. We carry out proper due diligence and carefully vet every real estate project with our governing
+                body, we regulate the process to ensure risks are mitigated to the bear minimum and project is safe for investment.
+              </p>
 
-              <span class="header-underline justify-content-end ml-auto"></span>
-            </span>
-          </span>
+              <main-button type="filled" text="Invest Now" size="md" />
+            </div>
+
+            <div style="position: relative; margin-top: -200px;" class="d-lg-block d-md-none col-lg-5  ">
+              <img id="co_invest" src="@/assets/images/co-invest.svg" width="600" height="600" class=" mt-n5" />
+            </div>
+            <div style="position: relative; margin-top: 80px" class="d-lg-none d-md-block col-md-12 ">
+              <img src="@/assets/images/co-invest.svg" width="700" height="700" class=" mt-n5" />
+            </div>
+          </div>
         </div>
       </section>
+
+      <section style="margin-top: 250px; margin-left: -30px">
+        <div class="container-fluid">
+          <div class="row ">
+            <div class="col-lg-5 col-md-12">
+              <div style="position: relative;" class="  ">
+                <img src="@/assets/images/co-own.svg" width="600" height="600" class=" mt-n5" />
+              </div>
+            </div>
+
+            <div id="co_own" class="col-lg-6  col-md-10  ml-md-5 mt-md-4 mt-lg-0 ml-lg-5">
+              <span class="d-flex flex-column text-align-right ">
+                <h3>Co - Own</h3>
+                <span class="header-underline ml-auto"></span>
+              </span>
+              <p id="co_own_text" class="w-100 mt-3 text-align-right co-own font-weight-normal font-poppins" style="line-height:40px">
+                Earn short-term returns when you invest in our vetted fractional real estate’s project. We carry out proper due diligence and carefully vet every real estate project with our governing
+                body, we regulate the process to ensure risks are mitigated to the bear minimum and project is safe for investment.
+              </p>
+
+              <main-button class="float-right" type="filled" text="Invest Now" size="md" />
+              <img id="co_own_circle" src="@/assets/svgs/light-circle.svg" width="400" height="400" class=" position-relative d-md-none d-lg-block  " style="z-index: -1; margin-top: -100px;" />
+              <img src="@/assets/svgs/light-circle.svg" width="300" height="300" class="d-md-block d-lg-none " style="margin-top: -100px" alt="" />
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <section style="margin-top: 150px;" class="ml-5">
+        <div class="container-fluid">
+          <div class="row ">
+            <div class="col-lg-6  col-md-11  mr-5 d-lg-block d-md-none ">
+              <span class="d-flex flex-column align-items-end  ">
+                <h3>Smart Invest</h3>
+                <span class="header-underline" style="margin-left: 150px;"></span>
+              </span>
+              <p id="smart_text" class="w-100 mt-3  text-align-right  font-weight-normal font-poppins" style="line-height:40px">
+                Smart Invest is an Automated Investment Option that allows you to shape your finances the way you want and allow our technology handle the rest. You can start building your finances
+                with a minimum of
+                <span class="text-main-red">5000 Naira</span>
+              </p>
+              <img src="@/assets/svgs/light-circle.svg" width="400" height="400" class=" position-relative d-md-none d-lg-block  " style="z-index: -1; margin-top: -300px;margin-left: 200px" />
+
+              <main-button type="filled" class="float-right mt-4" text="Learn More" size="md" />
+            </div>
+
+            <div class=" col-md-11 d-md-block d-lg-none   ">
+              <span class="d-flex flex-column align-items-end  ">
+                <h3>Smart Invest</h3>
+                <span class="header-underline" style="margin-left: 150px;"></span>
+              </span>
+              <p class="w-100 mt-3  text-align-right  font-weight-normal font-poppins" style="line-height:40px">
+                Smart Invest is an Automated Investment Option that allows you to shape your finances the way you want and allow our technology handle the rest. You can start building your finances
+                with a minimum of
+                <span class="text-main-red">5000 Naira</span>
+              </p>
+              <main-button type="filled" class="float-right mt-4" text="Learn More" size="md" />
+            </div>
+
+            <div style="position: relative; margin-top: -100px;" class="d-lg-block d-md-none col-lg-5  ">
+              <img id="smart_invest" src="@/assets/images/smart-invest.svg" width="600" height="600" class=" mt-n5" />
+            </div>
+            <div style="position: relative; margin-top: 80px" class="d-lg-none d-md-block col-md-12 ">
+              <img src="@/assets/images/smart-invest.svg" width="500" height="500" class=" mt-n5" />
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <section style="margin-top: 150px;" class="ml-5">
+        <div class="border-left-red d-flex flex-column  " style="margin-top: -120px">
+          <span class="text-main-red ">Benefits</span>
+          <span>Why you should choose us</span>
+        </div>
+      </section>
+
+      <section style="margin-top: 100px; position: relative" class="ml-5">
+        <div class="container-fluid  ">
+          <div class="row">
+            <div class="col-lg-4 col-md-4">
+              <div class="d-flex flex-column">
+                <img style="width: 100%!important;" width="100" height="100" src="@/assets/svgs/benefits_1.svg" />
+                <p class="text-center mt-5 font-weight-bold">Access to Profitable Projects</p>
+                <p class="font-weight-light">By venturing into fractional Real Estate with us, your wealth can grow in ways your savings can’t dream of.</p>
+              </div>
+            </div>
+            <div class="col-lg-4 col-md-4">
+              <div class="card shadow-3" style="border-color: white;border-radius: 20px;">
+                <div class="card-body">
+                  <div class="d-flex flex-column align-items-start">
+                    <img width="100" height="100" src="@/assets/svgs/benefit_2.svg" />
+                    <p class="text-center mt-5 font-weight-bold">Diversified Portfolio</p>
+                    <p class="font-weight-light">Investing through myyinvest guarantees secured returns and at a regular basis.</p>
+                  </div>
+                </div>
+              </div>
+              <img src="@/assets/svgs/lighte-red-circle.svg" width="300" height="300" class=" position-relative d-md-none d-lg-block  " style="z-index: -1; margin-top: -150px;margin-left: 150px" />
+
+              <!--              <div class="d-flex flex-column">-->
+              <!--                -->
+              <!--                <img style="width: 100%!important;" width="100" height="100" src="@/assets/svgs/benefits_1.svg">-->
+              <!--                <p class="text-center mt-5 font-weight-bold">Access to Profitable Projects</p>-->
+              <!--                <p>By venturing into fractional Real Estate with us,-->
+              <!--                  your wealth can grow in ways your savings can’t-->
+              <!--                  dream of.</p>-->
+              <!--              </div>-->
+            </div>
+            <div class="col-lg-4 col-md-4">
+              <div class="d-flex flex-column">
+                <img style="width: 100%!important;" width="100" height="100" src="@/assets/svgs/benefits_3.svg" />
+                <p class="text-center mt-5 font-weight-bold">Diversified Portfolio</p>
+                <p class="font-weight-light">With myyinvest, you get access to exclusive fractional real estate properties across Africa which is monitored from your personalized dashboard.</p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <img style="width: 100%!important;position:absolute;z-index: -1; top:20%!important;" src="@/assets/svgs/line-path.svg" />
+      </section>
+
+      <section style="margin-top: 200px;" class="ml-5">
+        <div class="border-left-red d-flex flex-column  " style="margin-top: -120px">
+          <span class="text-main-red ">SAFETY AND SECURITY</span>
+          <span>Why we are Reliable</span>
+        </div>
+      </section>
+
+      <section style="margin-top: 100px; position: relative" class="ml-5">
+        <div class="container-fluid  ">
+          <div class="row">
+            <div class="col-lg-4 col-md-4">
+              <div class="card shadow-3" style="border-bottom: 5px solid red;  border-radius: 20px;">
+                <div class="card-body">
+                  <div class="d-flex flex-column align-items-middle">
+                    <img width="100" height="100" class="mx-auto" src="@/assets/svgs/security_asset.svg" />
+                    <p class="text-center mt-5 font-weight-bold">ASSET MANAGEMENT</p>
+                    <p class="font-weight-light  text-center">
+                      Assets are held in Trust by Quantum Zenith Trustees, a Trustee arm of Zenith Bank and an entity regulated by Securities and Exchange Commission, Nigeria (SEC). Assets and Legal
+                      Documents are held by the Trustees.
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="col-lg-4 col-md-4 mt-5">
+              <div class="card shadow-3" style="border-bottom: 5px solid red;  border-radius: 20px;">
+                <div class="card-body">
+                  <div class="d-flex flex-column align-items-middle">
+                    <img width="100" height="100" class="mx-auto" src="@/assets/svgs/eye.svg" />
+                    <p class="text-center mt-5 font-weight-bold">SECURE ENCRYPTION</p>
+                    <p class="font-weight-light  text-center">
+                      Assets are held in Trust by Quantum Zenith Trustees, a Trustee arm of Zenith Bank and an entity regulated by Securities and Exchange Commission, Nigeria (SEC). Assets and Legal
+                      Documents are held by the Trustees.
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="col-lg-4 col-md-4">
+              <div class="card shadow-3" style="border-bottom: 5px solid red;  border-radius: 20px;">
+                <div class="card-body">
+                  <div class="d-flex flex-column align-items-middle">
+                    <img width="100" height="100" class="mx-auto" src="@/assets/svgs/shield.svg" />
+                    <p class="text-center mt-5 font-weight-bold">BANK GRADE SECURITY</p>
+                    <p class="font-weight-light  text-center">
+                      Assets are held in Trust by Quantum Zenith Trustees, a Trustee arm of Zenith Bank and an entity regulated by Securities and Exchange Commission, Nigeria (SEC). Assets and Legal
+                      Documents are held by the Trustees.
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <section style="margin-top: 200px;" class="ml-5">
+        <div class="border-left-red d-flex flex-column  " style="margin-top: -120px">
+          <span class="text-main-red ">OUR PARTNERS</span>
+          <span>Collaborators</span>
+        </div>
+        <p class="font-weight-light mt-2">We vet, Manage and Secure our investment opportunities alongside with;</p>
+      </section>
+
+      <section class="ml-5">
+        <div class="container-fluid">
+          <div class="row ">
+            <div class="col-lg-4 col-md-4">
+              <img width="200" height="200" class="mx-auto d-lg-none d-md-block" src="@/assets/partners/riby.svg" />
+              <img width="300" height="200" class="mx-auto d-lg-block d-md-none" src="@/assets/partners/riby.svg" />
+            </div>
+            <div class="col-lg-4 col-md-4">
+              <img width="200" height="200" class="mx-auto d-lg-none d-md-block" src="@/assets/partners/quantum.svg" />
+              <img width="300" height="200" class="mx-auto d-lg-block d-md-none" src="@/assets/partners/quantum.svg" />
+            </div>
+            <div class="col-lg-4 col-md-4">
+              <img width="200" height="200" class="mx-auto d-lg-none d-md-block" src="@/assets/partners/leadway.svg" />
+              <img width="300" height="200" class="mx-auto d-lg-block d-md-none" src="@/assets/partners/leadway.svg" />
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <section style="margin-top: 200px;" class="ml-5">
+        <div class="border-left-red d-flex flex-column  " style="margin-top: -120px">
+          <span class="text-main-red ">TESTIMONIES</span>
+          <span>What Our Clients Say</span>
+        </div>
+        <p class="font-weight-light mt-2">We cherish this kind words from our investors and we are glad to be part on this journey with them.</p>
+      </section>
+
+      <footer>
+        <bottom-footer />
+      </footer>
     </main>
 
     <main class="d-lg-none d-md-none d-block"></main>
@@ -78,10 +306,32 @@
 import TopNav from "../../components/Shared/TopNav";
 import MainButton from "../../components/form/MainButton";
 import People from "../../components/Shared/people";
+import BottomFooter from "../../components/Shared/bottomFooter";
 export default {
   name: "Landing",
-  components: { People, MainButton, TopNav }
+  components: { BottomFooter, People, MainButton, TopNav }
 };
 </script>
 
-<style scoped></style>
+<style scoped lang="scss">
+@media only screen and (min-width: 1024px) and (max-width: 1240px) {
+  /** {*/
+  /*  background-color: red!important;*/
+  /*}*/
+  #co_invest {
+    width: 400px !important;
+  }
+
+  #co_own_text {
+    width: 70% !important;
+    margin-left: 30% !important;
+  }
+  #co_own_circle {
+    margin-top: -200px !important;
+  }
+
+  #smart_invest {
+    width: 400px !important;
+  }
+}
+</style>

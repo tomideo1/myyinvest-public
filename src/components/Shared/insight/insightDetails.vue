e11d420d5fa54347d3d752b929beac5cfcc282d8
tomideo1 <ayotomideaina@gmail.com>
2021-04-29 23:11:42 +0100

    insights single

--- src/components/Shared/insight/insightDetails.vue
+++ src/components/Shared/insight/insightDetails.vue
@@ -90,19 +90,16 @@
             <header>
               <h3 class="h6">Leave a reply</h3>
             </header>
-            <form action="#" class="commenting-form">
+            <form @submit.prevent>
               <div class="row">
-                <div class="form-group col-md-6">
-                  <input type="text" name="username" id="username" placeholder="Name" class="form-control" />
-                </div>
-                <div class="form-group col-md-6">
-                  <input type="email" name="username" id="useremail" placeholder="Email Address (will not be published)" class="form-control" />
+                <div class="form-group col-md-12">
+                  <input type="email" v-model="form.commentEmail" placeholder="Email Address (will not be published)" class="form-control" />
                 </div>
                 <div class="form-group col-md-12">
-                  <textarea name="usercomment" id="usercomment" placeholder="Type your comment" class="form-control"></textarea>
+                  <textarea name="usercomment" v-model="form.commentText" id="usercomment" placeholder="Type your comment" class="form-control"></textarea>
                 </div>
                 <div class="form-group col-md-12">
-                  <main-button text="Submit Content" type="filled" class="col-md-12 mt-3" />
+                  <main-button :disabled="isLoading" @click="onSubmit" :text="buttonText" type="filled" class="col-md-12 mt-3" />
                 </div>
               </div>
             </form>
@@ -115,19 +112,58 @@
 
 <script>
 import MainButton from "@/components/form/MainButton";
+import { mapActions, mapGetters } from "vuex";
 import moment from "moment";
 export default {
+  data() {
+    return {
+      isLoading: false,
+      buttonText: "Post Comment",
+      form: { commentEmail: "", commentText: " ", id: this.$route.params.id }
+    };
+  },
   props: {
     insight: {
       type: Object
     }
   },
   components: { MainButton },
+  methods: {
+    ...mapActions(["postComment"]),
+    async onSubmit() {
+      this.isLoading = true;
+      const res = await this.postComment(this.form);
+      if (res) {
+        this.isLoading = false;
+        this.form = {};
+      } else {
+        this.isLoading = false;
+      }
+    }
+  },
   computed: {
+    ...mapGetters(["fetchInsights"]),
     formatDate() {
       return moment(this.insight.createdAt).format("MMMM Do YYYY, h:mm:ss a");
+    },
+    insights() {
+      const insights = this.fetchInsights;
+      let insightPost = { previousPost: null, nextPost: null };
+      for (let id = 0; id < insights.length; id++) {
+        if (insights[id]._id === this.$route.params.id) {
+          if (id === 0) {
+            insightPost = { previousPost: null, nextPost: { id: insights[id + 1] } };
+          } else if (id + 1 === this.fetchInsights.length) {
+            insightPost = { previousPost: { id: insights[id - 1] }, nextPost: null };
+          } else {
+            insightPost = { previousPost: { id: insights[id - 1] }, nextPost: { id: insights[id + 1] } };
+          }
+        }
+      }
+      return insights;
     }
-  }
+  },
+  mounted() {}
 };
 </script>
 

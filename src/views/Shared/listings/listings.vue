1010ab23c4a2c818f72762d62f76140339c9214d
Seth250 <ayowaleakintayo@gmail.com>
2021-05-19 22:52:45 +0100

    updated listings detail

--- src/views/Shared/listings/listings.vue
+++ src/views/Shared/listings/listings.vue
@@ -39,11 +39,10 @@
         </div>
       </div>
     </section> -->
-    <section class="container-fluid">
+    <section class="container-fluid list-container">
       <div class="row  ">
-        <!-- <div class="col-md-6 col-12 col-lg-4  listings-col " id="all" v-for="i in 6" :key="i"> -->
         <div class="col-md-6 col-12 col-lg-4  listings-col " id="all" v-for="i in 3" :key="i">
-          <div class="p-4">
+          <div class="p-2">
             <div class="single-list" style="border-radius:20px!important;">
               <div class="list-image">
                 <img src="https://res.cloudinary.com/dwpu7jpku/image/upload/v1612297295/listings_mfl4io.png" class="img-fluid" alt="" />
@@ -53,8 +52,8 @@
                 <p class="ft-18 w-100">{{ currentPlan(i).title }}</p>
                 <div class="row">
                   <div class="col-lg-6 col-md-6 col-10 " style="border-right:1px solid red;">
-                    <span class="d-flex flex-lg-row  ">
-                      <img src="https://res.cloudinary.com/myyinvest/image/upload/v1614000931/mmyyinvest-2.0/svgs/map_ktif6c.png/naira.svg" width="15" height="15" class="ml-3 mt-2" />
+                    <span class="d-flex flex-lg-row pl-3">
+                      <!-- <img src="https://res.cloudinary.com/myyinvest/image/upload/v1614000931/mmyyinvest-2.0/svgs/map_ktif6c.png/naira.svg" width="15" height="15" class="ml-3 mt-2" /> -->
                       <!-- <p class="ml-1 18 text-main-red font-weight-bolder">10,000</p> -->
                       <p class="text-main-red" style="font-weight: 600;">N{{ currentPlan(i).minInvest }}</p>
                     </span>
@@ -66,7 +65,7 @@
                     <!-- <p class="font-weight-light ft-12">
                       Lorem ipsum dolor sit amet consect etur adipisicing elit. Eligendi tempori bus libero at ipsum, eveniet maiores.
                     </p> -->
-                    <p class="font-weight-normal ft-12">{{ currentPlan(i).details.slice(0, 120) }}...</p>
+                    <p class="font-weight-normal ft-12">{{ currentPlan(i).details.slice(0, 65) }}...</p>
                     <hr class="mt-n2 ml-auto " style="border-color:red!important; position: relative; left: -7%" />
                     <!-- <main-button @click="$router.push({ name: 'single-listing' })" style="font-size: 10px!important;" text="Read More" size="xs" class="float-right" /> -->
                     <!-- <main-button @click="$router.push({ name: 'single-listing', params: { slug: slugs[i - 1] } })" style="font-size: 10px!important;" text="Read More" size="xs" class="float-right" /> -->
@@ -150,5 +149,10 @@ export default {
   top: 8px;
   left: 10px;
   position: absolute;
+}
+
+.list-container {
+  position: relative;
+  top: -2.5em;
 }
 </style>
